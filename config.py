170d82a2d153a049efe8d70237dec4e66fd902aa
dsokolovskiy <souldead147@gmail.com>
2024-09-18 15:33:43 +0300

    Add configuration file for storing technology list & other settings

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
-DJINNI_URL = "https://djinni.co/jobs/?primary_keyword=python"
+TECHNOLOGIES = [
+    "Django",
+    "Flask",
+    "PostgreSQL",
+    "MySQL",
+    "JavaScript",
+    "React",
+    "Vue",
+    "Docker",
+    "Kubernetes",
+    "AWS",
+    "Azure",
+]
+
+SCRAPE_CONFIG = {
+    "BASE_URL": "https://jobs.dou.ua/vacancies/",
+    "SEARCH_QUERY": "python",
+    "MAX_PAGES": 50,
+}
